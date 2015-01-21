be84734e9de0301bd0d26da7bdfa8256e4cc0990
Tong <a866559@gmail.com>
2015-01-21 23:09:39 +0800

    Add Process.cs

---
+++ Lvr_Land_Maker/Process.cs
@@ -0,0 +1,30 @@
+﻿using Lvr_Land_Maker.BLL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lvr_Land_Maker
+{
+    public class Process
+    {
+        private DataTable DataBaseTableFormat = null;
+
+        public Process()
+        {
+            if (DataBaseTableFormat == null)
+            {
+                DataBaseTableFormat = LandMakerHelper.CreateLandColumnDataTable();
+            }
+
+            
+        }
+
+        public void LandMakerProcess(string filesPath)
+        {
+
+        }
+    }
+}
