e9ca9ca63bbd7796efda3928d2e8263fc259ed7c
Tong <a866559@gmail.com>
2015-01-21 23:12:03 +0800

    Add LandFileDetailInfo Model

---
+++ Lvr_Land_Maker/Models/Enum/SaleType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lvr_Land_Maker.Models.Enum
+{
+    public enum SaleType
+    {
+        none = -1,
+
+        /// <summary>
+        /// 買賣
+        /// </summary>
+        Sale = 0,
+
+        /// <summary>
+        /// 預售
+        /// </summary>
+        PreOrder = 1,
+
+        /// <summary>
+        /// 租賃
+        /// </summary>
+        Leasing = 2
+    }
+}
---
+++ Lvr_Land_Maker/Models/LandFileDetailInfo.cs
@@ -0,0 +1,35 @@
+﻿using Lvr_Land_Maker.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Lvr_Land_Maker.Models
+{
+    public class LandFileDetailInfo
+    {
+        public string FileName { get; set; }
+
+        public SaleType SaleType { get; set; }
+
+        [XmlElement("TransactionNumber")]
+        public int Uid { get; set; }
+
+        [XmlElement("CityCode")]
+        public string CityCode { get; set; }
+
+        [XmlElement("City")]
+        public string CityName { get; set; }
+
+        [XmlElement("ZipCode")]
+        public int ZipCode { get; set; }
+
+        [XmlElement("TownshipCode")]
+        public string TownshipCode { get; set; }
+
+        [XmlElement("Township")]
+        public string Townships { get; set; }
+    }
+}
